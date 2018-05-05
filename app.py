3fc2bdd66c0bc42b8e8984dcb972ce5a32581882
Kristian Klemets <kristian.klemets@gmail.com>
2018-05-05 12:23:54 +0200

    Rework for Heroku

--- app.py
+++ app.py
@@ -1,58 +1,46 @@
-import os
-import sys
-import json
-import urllib2
-from datetime import datetime
+#!/usr/bin/env python3
+# -*- encoding: utf8 -*-
 
-import requests
+import os
+import urllib2
+import json
+import telegram
 from flask import Flask, request
+
 
 app = Flask(__name__)
 
+global bot
+bot = telegram.Bot(token=os.environ["TOKEN"])
 
-@app.route('/', methods=['GET'])
-def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
+@app.route('/asd123jaa', methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
 
-    return "Hello world", 200
+        chat_id = update.message.chat.id
 
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text.encode('utf-8')
 
-@app.route('/', methods=['POST'])
-def webhook():
+        # repeat the same message back (echo)
+        bot.sendMessage(chat_id=chat_id, text=get_image_url(text))
 
-    # endpoint for processing incoming messaging events
+    return 'ok'
 
-    data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('https://vavabot.herokuapp.com/asd123jaa')
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
 
-    if data["object"] == "page":
+@app.route('/')
+def index():
+    return 'Hello World!'
 
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    img_url = get_image_url(message_text)
-                    send_message(sender_id, img_url)
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
-
-    return "ok", 200
 
 def get_image_url(search_term):
     try:
@@ -66,45 +54,6 @@ def get_image_url(search_term):
         return j["items"][0]["link"]
     except Exception as e:
         print("Exception: " + str(e))
-
-def send_message(recipient_id, img_url):
-
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=img_url))
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message":{
-            "attachment":{
-                "type":"image", 
-                "payload":{
-                    "url": img_url, 
-                    "is_reusable": "true"
-                }
-            }
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
-
-def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
-    try:
-        if type(msg) is dict:
-            msg = json.dumps(msg)
-        print (u"{}: {}".format(datetime.now(), msg))
-    except Exception as e:
-        print(e)
-    sys.stdout.flush()
 
 
 if __name__ == '__main__':
