4cf462d7a611eef37fd79668240b315059527020
Kristian Klemets <kristian.klemets@gmail.com>
2018-05-06 21:51:59 +0200

    Testing own TGBot implementation

--- app.py
+++ app.py
@@ -3,33 +3,96 @@
 import os
 import urllib2
 import json
-import telegram
+import tgbot
 from flask import Flask, request
 from random import randint
+
+
+class GoogleImageBot:
+    def __init__(self):
+        self.update_offset = 0
+
+    def handleUpdate(self, update):
+        upid = update['update_id']
+        try:
+            msg = update['message']
+        except Exception as e:
+            print("Exception: " + str(e))
+        else:
+            self.handleMessage(msg)
+        self.update_offset = upid + 1
+
+    def handleMessage(self, msg):
+        if 'text' in msg:
+            text = msg['text']
+            commands = {
+                '/img': self.cmdImg
+            }
+            try:
+                cmdname, args = text.split(' ', 1)
+            except ValueError:
+                cmdname = text
+                args = ''
+            if cmdname in commands:
+                commands[cmdname](args, msg['chat'])
+
+    def cmdImg(self, query, chat_id):
+        #Get image and save in file
+        flag = True
+        idx = 0
+        #Loop finding an image that does not give 403 Forbidden error
+        while flag:
+            try:
+                url = self.get_image_url(query, idx)
+                if(url == None):
+                    break
+                filename = "out" + str(randint(0, 10000000000000000)) + ".jpg"
+                f = open(filename, 'wb')
+                f.write(urllib2.urlopen(url).read())
+                f.close()
+                flag = False
+            except urllib2.HTTPError:
+                idx += 1
+
+        if(flag == False):
+            #Send image and remove saved file
+            img = open(filename, 'rb')
+            bot.sendPhoto(chat_id=chat_id, photo=img)
+            img.close()
+            os.remove(filename)
+
+    def get_image_url(self, search_term, idx):
+        #Use Google Custom Search API to find an image
+        try:
+            key = os.environ["G_KEY"]
+            cx = os.environ["G_CX"]
+            searchType = "image"
+            gl = "fi"
+            search_term = search_term.replace(" ", "+")
+            url = "https://www.googleapis.com/customsearch/v1?q=" + search_term + "&key=" + key + "&cx=" + cx + "&searchType=" + searchType + "&gl=" + gl
+            contents = urllib2.urlopen(url).read()
+            j = json.loads(contents)
+            if("items" in j):
+                if(len(j["items"]) > idx):
+                    return j["items"][idx]["link"]
+            return None
+        except Exception as e:
+            print("Exception: " + str(e))
 
 
 app = Flask(__name__)
 
 global bot
-bot = telegram.Bot(token=os.environ["TOKEN"])
+global gimgbot
+bot = tgbot.TgbotConnection(os.environ["TOKEN"])
+gimgbot = GoogleImageBot()
 
 @app.route('/' + os.environ["HOOK"], methods=['POST'])
 def webhook_handler():
     if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat.id
-        text = update.message.text.encode('utf-8')
-
-        commands = {
-            '/img': cmdImg
-        }
-        try:
-            cmdname, args = text.split(' ', 1)
-        except ValueError:
-            cmdname = text
-            args = ''
-        if cmdname in commands:
-            commands[cmdname](args, chat_id)
+        updates = bot.getUpdates(offset=0, timeout=60)
+        for update in updates:
+            gimgbot.handleUpdate(update)
     return 'ok'
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
@@ -43,49 +106,6 @@ def set_webhook():
 @app.route('/')
 def index():
     return 'Hello World!'
-
-def cmdImg(query, chat_id):
-    #Get image and save in file
-    flag = True
-    idx = 0
-    #Loop finding an image that does not give 403 Forbidden error
-    while flag:
-        try:
-            url = get_image_url(query, idx)
-            if(url == None):
-                break
-            filename = "out" + str(randint(0, 10000000000000000)) + ".jpg"
-            f = open(filename, 'wb')
-            f.write(urllib2.urlopen(url).read())
-            f.close()
-            flag = False
-        except urllib2.HTTPError:
-            idx += 1
-            
-    if(flag == False):
-        #Send image and remove saved file
-        img = open(filename, 'rb')
-        bot.sendPhoto(chat_id=chat_id, photo=img)
-        img.close()
-        os.remove(filename)
-
-def get_image_url(search_term, idx):
-    #Use Google Custom Search API to find an image
-    try:
-        key = os.environ["G_KEY"]
-        cx = os.environ["G_CX"]
-        searchType = "image"
-        gl = "fi"
-        search_term = search_term.replace(" ", "+")
-        url = "https://www.googleapis.com/customsearch/v1?q=" + search_term + "&key=" + key + "&cx=" + cx + "&searchType=" + searchType + "&gl=" + gl
-        contents = urllib2.urlopen(url).read()
-        j = json.loads(contents)
-        if("items" in j):
-            if(len(j["items"]) > idx):
-                return j["items"][idx]["link"]
-        return None
-    except Exception as e:
-        print("Exception: " + str(e))
 
 
 if __name__ == '__main__':
---
+++ tgbot.py
@@ -0,0 +1,56 @@
+# -*- encoding: utf8 -*-
+
+import requests
+
+
+class TgbotConnection:
+    REQUEST_TIMEOUT = 30
+    def __init__(self, token):
+        self.token = token
+
+    def apiurl(self, method):
+        return 'https://api.telegram.org/bot{}/{}'.format(self.token, method)
+
+    def makeRequest(self, reqname, **params):
+        retries = 0
+        while True:
+            retries += 1
+            try:
+                response = requests.get(self.apiurl(reqname),
+                    params=params, timeout=self.REQUEST_TIMEOUT)
+            except requests.exceptions.ConnectionError as ex:
+                print('Connection error ({}) for  {} (try #{}), params: {}'.format(
+                    ex, reqname, retries, params))
+                continue
+            except requests.exceptions.Timeout:
+                continue
+            response.encoding = 'utf-8'
+            try:
+                json = response.json()
+            except TypeError:
+                json = response.json
+            except ValueError:
+                json = response.json
+            return json['result']
+
+    def getMe(self):
+        return self.makeRequest('getMe')
+
+    def getUpdates(self, offset=None, limit=None, timeout=None):
+        updates = self.makeRequest('getUpdates', offset=offset, limit=limit, timeout=timeout)
+        if updates is None:
+            return []
+        return updates
+
+    def sendMessage(self, chat_id, text):
+        return self.makeRequest('sendMessage', chat_id=chat_id, text=text)
+
+    def forwardMessage(self, chat_id, from_id, msg_id):
+        return self.makeRequest('forwardMessage', chat_id=chat_id,
+            from_chat_id=from_id, message_id=msg_id)
+
+    def sendPhoto(self, chat_id, photo):
+        return self.makeRequest('sendPhoto', chat_id=chat_id, photo=photo)
+
+    def setWebhook(self, url):
+        return self.makeRequest('setWebhook', url=url)
