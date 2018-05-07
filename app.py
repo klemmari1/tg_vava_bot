0e7391df9791eb28e7dc1bc8e00dc52e4ec50c71
Kristian Klemets <kristian.klemets@gmail.com>
2018-05-07 11:26:23 +0200

    Try sending error image from web

--- app.py
+++ app.py
@@ -4,14 +4,20 @@ import os
 import urllib2
 import json
 import tgbot
+import random
 from flask import Flask, request
-from random import randint
 
 
 app = Flask(__name__)
 
 global bot
 bot = tgbot.TgbotConnection(os.environ["TOKEN"])
+
+error_images = {"https://cdnb.artstation.com/p/assets/images/images/001/993/973/large/benjamin-eberhard-sad-robot.jpg?1455679192",
+                "http://img03.deviantart.net/43d5/i/2016/063/8/4/detective_sad_robot_by_spikesthecat-d9tv8se.png",
+                "https://c1.staticflickr.com/3/2735/4262821422_b9b5a523c6.jpg",
+                "https://musewb.files.wordpress.com/2012/10/5152157159_5ba65100a9_z.jpg"
+                "https://fsmedia.imgix.net/83/3a/80/8b/8f3c/427e/aac2/99891d289d7c/the-robot-sophia-makes-an-angry-face-during-an-on-stage-interview.png?auto=format%2Ccompress&w=700"}
 
 @app.route('/' + os.environ["HOOK"], methods=['POST'])
 def webhook_handler():
@@ -61,21 +67,13 @@ def cmdImg(query, chat_id):
     url = get_image_url(query)
     if(url == -1):
         #Send image about daily limit reached
-        num = randint(0, 5)
-        filename = "images/" + str(num) + ".jpg"
-        img = open(filename, 'rb')
-        bot.sendPhoto(chat_id=chat_id, photo=img, caption="You done up reached my daily search limit again :(")
-        img.close()
+        bot.sendPhoto(chat_id=chat_id, photo=random.choice(error_images), caption="You done up reached my daily search limit again :(")
     elif(url != None):
         #If image found
         bot.sendPhoto(chat_id=chat_id, photo=url)
 
 def testImg(query, chat_id):
-    num = randint(0, 5)
-    filename = "images/" + str(num) + ".jpg"
-    img = open(filename, 'rb')
-    reply = bot.sendPhoto(chat_id=chat_id, photo=img, caption="You done up reached my daily search limit again :(")
-    img.close()
+    reply = bot.sendPhoto(chat_id=chat_id, photo=random.choice(error_images), caption="You done up reached my daily search limit again :(")
     print(str(reply))
 
 def get_image_url(search_term):
