cf524c5fee15245c5909dc038a927e7fce5869cf
Kristian Klemets <kristian.klemets@gmail.com>
2020-04-14 09:59:35 +0300

    Make everything black
    Formatted everything with python Black formatter.
    
    Also clean some error messages.

--- app.py
+++ app.py
@@ -14,41 +14,33 @@ app = Flask(__name__)
 
 bot = tgbot.TgbotConnection(os.environ["TOKEN"])
 
-error_images = ["https://cdnb.artstation.com/p/assets/images/images/001/993/973/large/benjamin-eberhard-sad-robot.jpg?1455679192",
-                "http://img03.deviantart.net/43d5/i/2016/063/8/4/detective_sad_robot_by_spikesthecat-d9tv8se.png",
-                "https://c1.staticflickr.com/3/2735/4262821422_b9b5a523c6.jpg",
-                "https://musewb.files.wordpress.com/2012/10/5152157159_5ba65100a9_z.jpg"
-                "https://fsmedia.imgix.net/83/3a/80/8b/8f3c/427e/aac2/99891d289d7c/the-robot-sophia-makes-an-angry-face-during-an-on-stage-interview.png?auto=format%2Ccompress&w=700"]
+error_images = [
+    "https://miro.medium.com/max/1400/1*pUEZd8z__1p-7ICIO1NZFA.png",
+]
 
-not_found_images = ["http://cdn.funnyand.com/wp-content/uploads/2014/03/404-PagenotfoundLOL_20140325-300x300.jpg",
-                    "http://www.profitguide.com/wp-content/uploads/2013/09/google-not-found.jpg",
-                    "https://i.imgflip.com/11fjj7.jpg",
-                    "https://sd.keepcalm-o-matic.co.uk/i/error-404-meme-not-found.png"]
+not_found_images = [
+    "https://howfix.net/wp-content/uploads/2018/02/sIaRmaFSMfrw8QJIBAa8mA-article.png",
+]
 
-not_found_captions = ["You're a special snowflake, aren't you? No image found!",
-                      "No image found!",
-                      "Sry, no image",
-                      "Cannot find images with this query!",
-                      "Googl does not hav dis",
-                      "No images with this query!",
-                      "Where image? Not in internet",
-                      "Hmm.. Cannot find this",
-                      "Not found on google.. Maybe you can put it there?",
-                      "Too weird query. Cannot find"]
+not_found_captions = [
+    "Your query did not match any images.",
+]
 
-@app.route('/' + os.environ["HOOK"], methods=['POST'])
+
+@app.route("/" + os.environ["HOOK"], methods=["POST"])
 def webhook_handler():
     if request.method == "POST":
         message = request.get_json(force=True)
         if "inline_query" in message:
-            inline_query = message['inline_query']
+            inline_query = message["inline_query"]
             handleInlineQuery(inline_query)
         else:
-            msg = message['message']
+            msg = message["message"]
             handleMessage(msg)
-    return 'ok'
+    return "ok"
 
-@app.route('/set_webhook', methods=['GET', 'POST'])
+
+@app.route("/set_webhook", methods=["GET", "POST"])
 def set_webhook():
     print("Host URL: " + request.host_url)
     s = bot.setWebhook(request.host_url + os.environ["HOOK"])
@@ -57,96 +49,112 @@ def set_webhook():
     else:
         return "webhook setup failed"
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return 'Hello World!'
+    return "Hello World!"
 
 
 def handleMessage(msg):
-    if 'text' in msg:
-        text = msg['text']
-        commands = {
-            '/img': cmdImg,
-            '/puppu': cmdPuppu,
-            '/vtest': testImg
-        }
+    if "text" in msg:
+        text = msg["text"]
+        commands = {"/img": cmdImg, "/puppu": cmdPuppu, "/vtest": testImg}
         try:
-            cmdname, args = text.split(' ', 1)
+            cmdname, args = text.split(" ", 1)
         except ValueError:
             cmdname = text
-            args = ''
+            args = ""
         if cmdname in commands:
             print("command: " + str(cmdname))
             print("args: " + str(args))
-            print("chat id: " + str(msg['chat']['id']))
-            commands[cmdname](args, msg['chat']['id'])
+            print("chat id: " + str(msg["chat"]["id"]))
+            commands[cmdname](args, msg["chat"]["id"])
+
 
 def handleInlineQuery(inline_query):
-    if 'query' in inline_query:
-        query = inline_query['query']
+    if "query" in inline_query:
+        query = inline_query["query"]
         if query != "":
-            inline_query_id = inline_query['id']
+            inline_query_id = inline_query["id"]
             print("inline query id: " + str(inline_query_id))
             print("inline query args: " + str(query))
             items = google_search(query)
-            if (isinstance(items, list)):
-                response = bot.sendInlineResponse(inline_query_id=inline_query_id, items=items)
-                if (response.status_code != 200):
+            if isinstance(items, list):
+                response = bot.sendInlineResponse(
+                    inline_query_id=inline_query_id, items=items
+                )
+                if response.status_code != 200:
                     print("Error sending inline response: " + str(response.text))
             else:
-                print("Error getting images from Google search. Response: " + str(items))
+                print(
+                    "Error getting images from Google search. Response: " + str(items)
+                )
+
 
 def cmdImg(query, chat_id):
-    #If empty query
-    if(not query):
+    # If empty query
+    if not query:
         return
-    #Get results with query
+    # Get results with query
     items = google_search(query)
-    #TODO check daily search quota
-    if(items == -1):
-        #Send image about daily limit reached
+    # TODO check daily search quota
+    if items == -1:
+        # Send image about daily limit reached
         dailyLimit(query, chat_id)
         return
-    elif(items == -2):
+    elif items == -2:
         return
-    elif(items == None):
-        #Send image about image not found
+    elif items == None:
+        # Send image about image not found
         notFound(query, chat_id)
         return
-    #Send image that does not give client errors
+    # Send image that does not give client errors
     for item in items:
         url = item["link"]
         response = bot.sendPhoto(chat_id=chat_id, photo=url)
-        if (response.status_code != 200):
+        if response.status_code != 200:
             print(str(response))
         else:
             break
 
+
 def cmdPuppu(query, chat_id):
-    query = urllib.parse.quote_plus(query, safe='', encoding='utf-8', errors=None)
+    query = urllib.parse.quote_plus(query, safe="", encoding="utf-8", errors=None)
     url = "http://puppulausegeneraattori.fi/?avainsana=" + query
     response = urllib.request.urlopen(url).read()
-    soup=BeautifulSoup(response, "html5lib")
-    text=soup.find('p', {"class": "lause"})
+    soup = BeautifulSoup(response, "html5lib")
+    text = soup.find("p", {"class": "lause"})
     text = text.contents[0]
     print(text)
     bot.sendMessage(chat_id=chat_id, text=text)
 
+
 def google_search(search_terms):
-    #Use Google Custom Search API to find an image
+    # Use Google Custom Search API to find an image
     try:
         key = os.environ["G_KEY"]
         cx = os.environ["G_CX"]
         searchType = "image"
         gl = "fi"
-        url = "https://www.googleapis.com/customsearch/v1?q=" + search_terms + "&key=" + key + "&cx=" + cx + "&searchType=" + searchType + "&gl=" + gl
+        url = (
+            "https://www.googleapis.com/customsearch/v1?q="
+            + search_terms
+            + "&key="
+            + key
+            + "&cx="
+            + cx
+            + "&searchType="
+            + searchType
+            + "&gl="
+            + gl
+        )
         contents = requests.get(url).text
         json_response = json.loads(contents)
-        if("items" in json_response):
+        if "items" in json_response:
             return json_response["items"]
-        elif("error" in json_response):
-            if("message" in json_response["error"]):
-                if("billing" in json_response["error"]["message"]):
+        elif "error" in json_response:
+            if "message" in json_response["error"]:
+                if "billing" in json_response["error"]["message"]:
                     return -1
         return None
     except urllib.error.HTTPError as e:
@@ -156,18 +164,29 @@ def google_search(search_terms):
         print("Exception: " + str(e))
         return -2
 
+
 def testImg(query, chat_id):
     if query == "1":
         print(dailyLimit(query, chat_id))
     elif query == "2":
         print(notFound(query, chat_id))
 
+
 def dailyLimit(query, chat_id):
-    return bot.sendPhoto(chat_id=chat_id, photo=random.choice(error_images), caption="You done reached my daily search limit again :(")
+    return bot.sendPhoto(
+        chat_id=chat_id,
+        photo=random.choice(error_images),
+        caption="You've reached the daily search limit of Google API :(",
+    )
+
 
 def notFound(query, chat_id):
-    return bot.sendPhoto(chat_id=chat_id, photo=random.choice(not_found_images), caption=random.choice(not_found_captions))
+    return bot.sendPhoto(
+        chat_id=chat_id,
+        photo=random.choice(not_found_images),
+        caption=random.choice(not_found_captions),
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- tgbot.py
+++ tgbot.py
@@ -7,49 +7,60 @@ import json
 
 class TgbotConnection:
     REQUEST_TIMEOUT = 30
+
     def __init__(self, token):
         self.token = token
 
     def apiurl(self, method):
-        return 'https://api.telegram.org/bot{}/{}'.format(self.token, method)
+        return "https://api.telegram.org/bot{}/{}".format(self.token, method)
 
     def makeRequest(self, reqname, **params):
         retries = 0
         while True:
             retries += 1
             try:
-                response = requests.get(self.apiurl(reqname),
-                    params=params, timeout=self.REQUEST_TIMEOUT)
+                response = requests.get(
+                    self.apiurl(reqname), params=params, timeout=self.REQUEST_TIMEOUT
+                )
             except requests.exceptions.ConnectionError as ex:
-                print('Connection error ({}) for  {} (try #{}), params: {}'.format(
-                    ex, reqname, retries, params))
+                print(
+                    "Connection error ({}) for  {} (try #{}), params: {}".format(
+                        ex, reqname, retries, params
+                    )
+                )
                 continue
             except requests.exceptions.Timeout:
                 continue
-            response.encoding = 'utf-8'
+            response.encoding = "utf-8"
             return response
 
     def sendMessage(self, chat_id, text):
-        return self.makeRequest('sendMessage', chat_id=chat_id, text=text)
+        return self.makeRequest("sendMessage", chat_id=chat_id, text=text)
 
     def sendPhoto(self, chat_id, photo, caption=None):
-        return self.makeRequest('sendPhoto', chat_id=chat_id, photo=photo, caption=caption)
+        return self.makeRequest(
+            "sendPhoto", chat_id=chat_id, photo=photo, caption=caption
+        )
 
     def setWebhook(self, url):
-        return self.makeRequest('setWebhook', url=url)
+        return self.makeRequest("setWebhook", url=url)
 
     def sendInlineResponse(self, inline_query_id, items):
         results = []
         for item in items:
             photo_url = item["link"]
             thumb_url = item["image"]["thumbnailLink"]
-            results.append(InlineQueryResultPhoto(photo_url=photo_url, thumb_url=thumb_url))
-        return self.makeRequest('answerInlineQuery', inline_query_id=inline_query_id, results=json.dumps([ob.__dict__ for ob in results]))
-
+            results.append(
+                InlineQueryResultPhoto(photo_url=photo_url, thumb_url=thumb_url)
+            )
+        return self.makeRequest(
+            "answerInlineQuery",
+            inline_query_id=inline_query_id,
+            results=json.dumps([ob.__dict__ for ob in results]),
+        )
 
 
 class InlineQueryResultPhoto:
-
     def __init__(self, photo_url, thumb_url):
         self.type = "photo"
         self.id = str(uuid.uuid4())
