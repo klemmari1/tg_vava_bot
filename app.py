93750cd8ed335ce8dbb1c3fbd077d685ecf1d155
Kristian Klemets <kristian.klemets@gmail.com>
2020-12-11 18:01:54 +0200

    Format change

--- app.py
+++ app.py
@@ -149,7 +149,11 @@ def cmdPuppu(query, chat_id):
 
 def cmdInspis(query, chat_id):
     url = "https://inspirobot.me/api?generate=true"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    headers = {
+        "User-Agent": """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)
+        AppleWebKit/537.36 (KHTML, like Gecko)
+        Chrome/50.0.2661.102 Safari/537.36"""
+    }
     response = requests.get(url, headers=headers)
     url = response.content.decode("utf-8")
     print(url)
