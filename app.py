7a910cd7703d39731991e5ae70012b00d770a916
Kristian Klemets <ykristian.klemets@gmail.com>
2024-04-09 21:14:28 +0300

    Fix sending out of context

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
 
 
 async def send_message_to_chat(chat_id: int, text: str) -> None:
-    await bot.bot(settings.TELEGRAM_TOKEN).send_message(
+    await bot.bot.send_message(
         chat_id=chat_id,
         text=text,
         disable_web_page_preview=True,
